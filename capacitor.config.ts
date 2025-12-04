374051834c49d6ef3b8bc507aa5eb8a94b1bbbe1
Bruno Alejandro Ortiz Velasquez <counter_19_95@hotmail.com>
2025-12-04 17:12:18 -0400

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.dev.cayudiapp',
+  appName: 'cayudiapp',
+  webDir: 'www'
+};
+
+export default config;
